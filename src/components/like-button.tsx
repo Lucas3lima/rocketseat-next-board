172398d0fd90ae7335feafebccadc9bf6766d4cd
lucas3lima <lucaslima.code@gmail.com>
2026-06-02 21:46:39 -0300

    atualização do cache

--- src/components/like-button.tsx
+++ src/components/like-button.tsx
@@ -1,10 +1,10 @@
-import { ComponentProps } from 'react'
+import type z from 'zod'
+import type { IssueInteractionsResponseSchema } from '@/api/routes/schemas/issue-interactions'
+import type { ComponentProps, MouseEvent } from 'react'
 import { Button } from './button'
 import { ThumbsUpIcon } from 'lucide-react'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { toggleLike } from '@/http/toggle-like'
-import type { IssueInteractionsResponseSchema } from '@/api/routes/schemas/issue-interactions'
-import z from 'zod'
 
 interface LikeButtonProps extends ComponentProps<'button'> {
   issueId: string
@@ -21,16 +21,16 @@ export function LikeButton({
   ...props
 }: LikeButtonProps) {
   const queryClient = useQueryClient()
-  const { mutate: handleToggleLike, isPending } = useMutation({
+  const { mutate: onToggleLike, isPending } = useMutation({
     mutationFn: () => toggleLike({ issueId }),
     onMutate: async () => {
-      const previousData = queryClient.getQueryData<IssueInteractionsResponse>([
-        'issue-likes',
-        issueId,
-      ])
+      const previousData =
+        queryClient.getQueriesData<IssueInteractionsResponse>({
+          queryKey: ['issue-likes'],
+        })
 
-      queryClient.setQueryData<IssueInteractionsResponse>(
-        ['issue-likes', issueId],
+      queryClient.setQueriesData<IssueInteractionsResponse>(
+        { queryKey: ['issue-likes'] },
         (old) => {
           if (!old) return undefined
 
@@ -55,7 +55,9 @@ export function LikeButton({
     },
     onError: async (err, _, context) => {
       if (context?.previousData) {
-        queryClient.setQueryData(['issue-likes', issueId], context.previousData)
+        for (const [queryKey, data] of context.previousData) {
+          queryClient.setQueryData<IssueInteractionsResponse>(queryKey, data)
+        }
       }
 
       return { error: err.message }
@@ -63,13 +65,19 @@ export function LikeButton({
   })
   const liked = initalLiked
 
+  function handleToggleLike(event: MouseEvent) {
+    event.preventDefault()
+    event.stopPropagation()
+
+    onToggleLike()
+  }
   return (
     <Button
-      data-liked={liked}
+      data-liked={initalLiked}
       className="data-[liked=true]:bg-indigo-600 data-[liked=true]:hover:bg-indigo-500 data-[liked=true]:text-white"
       aria-label={liked ? 'Unlike' : 'Like'}
       disabled={isPending}
-      onClick={() => handleToggleLike()}
+      onClick={handleToggleLike}
       {...props}
     >
       <ThumbsUpIcon className="size-3" />
